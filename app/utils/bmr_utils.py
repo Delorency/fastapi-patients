1ffc71957a139b2f0da5bd8ee3e0683810709f4c
delorency <delorency>
2025-11-01 16:43:31 +0300

    fix formula

--- app/utils/bmr_utils.py
+++ app/utils/bmr_utils.py
@@ -3,6 +3,6 @@ def generate_bmr_mif_sanj(height:int, weight:int,year:int, male:bool) -> float:
     return 10*weight + 6.25*height - 5*year - 161
 
 
-def generate_bmr_har_ben(year:int, height:int, weight:int, male:bool) -> float:
+def generate_bmr_har_ben(height:int, weight:int,year:int, male:bool) -> float:
     if male: return 66 + 13.7*weight + 5*height - 6.8*year
     return 655 + 9.6*weight + 1.8*height - 4.7*year
